51a4a7c69541eab04d7d1f1ed4b55bcdbcef3950
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-06-07 10:46:05 +0600

    apply validation

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -24,20 +24,26 @@ const Register = () => {
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
             <div>
               <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-700">Username</label>
-              <input type="text" {...register("username")} id="username" className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 ease-in-out" />
+              <input type="text" {...register("name", { required: true })} id="name" className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 ease-in-out" />
+              {errors.name && <span className='text-red-500'>Please fill up this field</span>}
             </div>
             <div>
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-700">Email</label>
-              <input type="email" {...register("email")} id="email" className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 ease-in-out" />
+              <input type="email" {...register("email", { required: true })} id="email" className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 ease-in-out" />
+              {errors.email && <span className='text-red-500'>Please fill up this field</span>}
             </div>
             <div>
               <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-700">Password</label>
-              <input type="password" {...register("password")} id="password" className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 ease-in-out" />
+              <input type="password" {...register("password", { required: true, minLength: 6, maxLength: 20 })} id="password" className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 ease-in-out" />
+              {errors.password?.type === "minLength" && (
+                <p className='text-red-500'>password must be 6 characters</p>
+              )}
             </div>
-            <div>
+            {/* <div>
               <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-700">Confirm Password</label>
-              <input type="password" {...register("confirm-password")} id="confirm-password" className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 ease-in-out" />
-            </div>
+              <input type="password" {...register("confirm-password", { required: true })} id="confirm-password" className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 ease-in-out" />
+              {errors.confirm-password && <span className='text-red-500'>Please fill up this field</span>}
+            </div> */}
             <button type="submit" className="w-full bg-[#27A3FA] text-white p-3 rounded-lg shadow-lg hover:bg-[#2784F5] focus:outline-none focus:ring-2 focus:ring-[#27A3FA] focus:ring-opacity-50 transition duration-300 ease-in-out">Register</button>
           </form>
           <div className="mt-6 text-center text-sm text-gray-600" >
