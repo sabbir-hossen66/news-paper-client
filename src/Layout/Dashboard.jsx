1716c4597313b80eb381ae4ad560649f701948fd
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-06-14 15:14:05 +0600

    icon added in the dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { FaUsersLine } from "react-icons/fa6";
+import { MdOutlineArticle } from "react-icons/md";
+import { SiGradleplaypublisher } from "react-icons/si";
 import { NavLink, Outlet } from "react-router-dom";
 
 
@@ -35,29 +38,31 @@ const Dashboard = () => {
           <ul className="pt-2 pb-4 space-y-1 text-sm">
             <li className="rounded-sm">
               <a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current dark:text-gray-600">
-                  <path d="M469.666,216.45,271.078,33.749a34,34,0,0,0-47.062.98L41.373,217.373,32,226.745V496H208V328h96V496H480V225.958ZM248.038,56.771c.282,0,.108.061-.013.18C247.9,56.832,247.756,56.771,248.038,56.771ZM448,464H336V328a32,32,0,0,0-32-32H208a32,32,0,0,0-32,32V464H64V240L248.038,57.356c.013-.012.014-.023.024-.035L448,240Z"></path>
-                </svg>
+                <FaUsersLine />
                 <NavLink to={'/dashboard/all-users'}><span>All Users</span></NavLink>
               </a>
             </li>
+
             <li className="rounded-sm">
               <a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current dark:text-gray-600">
-                  <path d="M479.6,399.716l-81.084-81.084-62.368-25.767A175.014,175.014,0,0,0,368,192c0-97.047-78.953-176-176-176S16,94.953,16,192,94.953,368,192,368a175.034,175.034,0,0,0,101.619-32.377l25.7,62.2L400.4,478.911a56,56,0,1,0,79.2-79.195ZM48,192c0-79.4,64.6-144,144-144s144,64.6,144,144S271.4,336,192,336,48,271.4,48,192ZM456.971,456.284a24.028,24.028,0,0,1-33.942,0l-76.572-76.572-23.894-57.835L380.4,345.771l76.573,76.572A24.028,24.028,0,0,1,456.971,456.284Z"></path>
-                </svg>
-                <span>Search</span>
+                <MdOutlineArticle />
+                <span>All Article</span>
               </a>
             </li>
+
             <li className="rounded-sm">
               <a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current dark:text-gray-600">
-                  <path d="M448.205,392.507c30.519-27.2,47.8-63.455,47.8-101.078,0-39.984-18.718-77.378-52.707-105.3C410.218,158.963,366.432,144,320,144s-90.218,14.963-123.293,42.131C162.718,214.051,144,251.445,144,291.429s18.718,77.378,52.707,105.3c33.075,27.168,76.861,42.13,123.293,42.13,6.187,0,12.412-.273,18.585-.816l10.546,9.141A199.849,199.849,0,0,0,480,496h16V461.943l-4.686-4.685A199.17,199.17,0,0,1,448.205,392.507ZM370.089,423l-21.161-18.341-7.056.865A180.275,180.275,0,0,1,320,406.857c-79.4,0-144-51.335-144-115.428s64.6-115.429,144-115.429,144,51.335,144,115.429c0,31.588-15.527,61.363-43.843,84.091L389.911,377l-19.822,46ZM504.971,456.284a24.028,24.028,0,0,1-33.942,0l-76.572-76.572-23.894-57.835L380.4,345.771l76.573,76.572A24.028,24.028,0,0,1,504.971,456.284Z"></path>
-                </svg>
-                <span>Messages</span>
+                <SiGradleplaypublisher />
+                <span>Add Publisher</span>
               </a>
             </li>
-            <p>or <hr /></p>
+
+            {/* divider is here */}
+            <div className="relative flex py-5 items-center">
+              <div className="flex-grow border-t border-gray-400"></div>
+              <span className="flex-shrink mx-4 text-white">OR</span>
+              <div className="flex-grow border-t border-gray-400"></div>
+            </div>
 
             <li className="rounded-sm">
               <a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
