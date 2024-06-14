fa607718422ae1d68f211b99287706852ab49543
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-06-14 18:14:26 +0600

    authproviders page added axiosPublic and localstorage

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,22 +1,39 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
+
   // TODO:USE AXIOS SECURE AT AXIOS PUBLIC
   const axiosPublic = useAxiosPublic();
-  const { data: users = [] } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ['users'],
     queryFn: async () => {
-      const res = await axiosPublic.get('/users');
+      const res = await axiosPublic.get('/users', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('access-token')}`
+        }
+      });
       return res.data
     }
   })
 
 
-  const handleMakeAdmin = (id) => {
-    // Logic to make the user an admin
-    console.log(`Making user with id ${id} an admin`);
+  const handleMakeAdmin = (user) => {
+    axiosPublic.patch(`/users/admin/${user._id}`)
+      .then(res => {
+        if (res.data.modifiedCount > 0) {
+          refetch()
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user?.name} is an admin now`,
+            showConfirmButton: false,
+            timer: 1500
+          });
+        }
+      })
   };
 
 
@@ -45,12 +62,16 @@ const AllUsers = () => {
                   <img src={user.photoURL} alt="Profile" className="w-10 h-10 rounded-full mx-auto lg:mx-0" />
                 </td>
                 <td className="px-4 py-2 whitespace-nowrap border-b border-gray-500">
-                  <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 lg:py-2 lg:px-4 rounded text-xs lg:text-sm"
-                    onClick={() => handleMakeAdmin(user.id)}
-                  >
-                    Make Admin
-                  </button>
+
+                  {
+                    user?.role === 'admin' ? 'Admin' :
+                      <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 lg:py-2 lg:px-4 rounded text-xs lg:text-sm"
+                        onClick={() => handleMakeAdmin(user)} >
+                        Make Admin
+                      </button>
+                  }
+
                 </td>
               </tr>
             ))}
--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,6 +1,7 @@
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.init";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 
 
@@ -9,7 +10,8 @@ export const AuthContext = createContext(null);
 
 const AuthProviders = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
 
   const createUser = (email, password, name) => {
     setLoading(true)
@@ -50,8 +52,21 @@ const AuthProviders = ({ children }) => {
 
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
-      // console.log(currentUser);
       setUser(currentUser)
+      if (currentUser) {
+        // get token and store client
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post('/jwt', userInfo)
+          .then(res => {
+            if (res.data.token) {
+              localStorage.setItem('access-token', res.data.token)
+            }
+          })
+      }
+      else {
+        // Todo:remove token 
+        localStorage.removeItem('access-token')
+      }
       setLoading(false)
     })
     return () => {
