6dde31c5b98049b27ce9e2e99f0012ba8d6f6167
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-06-16 02:32:35 +0600

    complete publisher component

--- src/components/Publishers/Publishers.jsx
+++ src/components/Publishers/Publishers.jsx
@@ -11,7 +11,7 @@ const Publishers = () => {
       const res = await axiosScure.get('/seePublisher');
       return res.data;
     },
-    // enabled: !!id,
+
   });
 
   return (
@@ -20,15 +20,19 @@ const Publishers = () => {
       <div>
         <div className="container mx-auto grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-8">
           {publishers.map((publisher, index) => (
+
             <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden">
+
               <img className="w-full h-48 object-cover" src={publisher.image} alt={publisher.publisher} />
               <div className="p-4">
-                <h3 className="text-xl font-semibold">Published by: {publisher.publisher ? publisher.publisher?.value : 'Unknown'}</h3>
+                <p className="text-xl font-semibold text-gray-500">Published by: {publisher.publisher ? publisher.publisher?.value : 'Unknown'}</p>
+
               </div>
             </div>
           ))}
         </div>
       </div>
+
 
 
     </div>
